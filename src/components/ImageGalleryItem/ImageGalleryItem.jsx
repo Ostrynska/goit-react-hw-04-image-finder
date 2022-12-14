4b6a1dabf484abaebe766e516e310b65a173b7b2
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-14 22:05:52 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,25 @@
+import PropTypes from 'prop-types';
+
 import {
   ImageGalleryItems,
   ImageGalleryImage,
 } from './ImageGalleryItem.styled';
 
-export const ImageGalleryItem = ({ id, tags, webImage, largeImage }) => {
+export const ImageGalleryItem = ({ url, tag, openModal, fullImage }) => {
   return (
     <ImageGalleryItems>
-      <ImageGalleryImage src={webImage} alt={tags} />
+      <ImageGalleryImage
+        src={url}
+        alt={tag}
+        onClick={() => openModal(fullImage)}
+      />
     </ImageGalleryItems>
   );
+};
+
+ImageGalleryItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  fullImage: PropTypes.string.isRequired,
+  openModal: PropTypes.func.isRequired,
 };
