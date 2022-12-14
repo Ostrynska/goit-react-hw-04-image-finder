d70bb4efd4105ce6688f1d37b9f22f24dad03d4c
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-14 22:06:19 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,28 +1,32 @@
 import axios from 'axios';
 
-import toast from 'react-hot-toast';
-
 const API_KEY = '29966303-d9893c98832d118b1f4c04955';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const getImages = async (searchName, page) => {
+export const getImages = async (q, page) => {
   const options = new URLSearchParams({
     key: `${API_KEY}`,
-    q: searchName,
+    q,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 12,
-    page: page,
+    page,
   });
   const URL = `${BASE_URL}?${options}`;
   try {
     const response = await axios.get(URL);
-    return response.data.hits;
+    return response.data;
   } catch (error) {
-    return toast(
-      `Nooooooooo we cannot find ${searchName}😢
-        Please try again`
-    );
+    console.log(error);
   }
 };
+
+export function getImageData(data) {
+  return data.map(({ id, tags, largeImageURL, webformatURL }) => ({
+    id,
+    tags,
+    largeImageURL,
+    webformatURL,
+  }));
+}
