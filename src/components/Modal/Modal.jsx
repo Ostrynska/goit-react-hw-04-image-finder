ed575f1aebe0cb4cb5eb7ffeb3d4d7fbf6f3a9fe
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-14 22:06:00 +0200

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Overlay, ModalWindow } from './Modal.styled';
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  onKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onModalClick();
+    }
+  };
+
+  onBackDropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onModalClick();
+    }
+  };
+
+  render() {
+    const { fullImage, alt } = this.props;
+
+    return (
+      <Overlay onClick={this.onBackDropClick}>
+        <ModalWindow>
+          <img src={fullImage} alt={alt} />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
+
+Modal.prototypes = {
+  fullImage: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  onModalClick: PropTypes.func.isRequired,
+};
