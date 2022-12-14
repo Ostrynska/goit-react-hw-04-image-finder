08651b05b2a69bb74429588fcbacf172612b9d46
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-14 22:05:44 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,18 +1,23 @@
+import PropTypes from 'prop-types';
+
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Gallery } from './ImageGallery.styled';
 
-export const ImageGallery = ({ images }) => {
-  return (
-    <Gallery>
-      {images.map(({ id, tags, webformatURL, largeImageURL }) => (
-        <ImageGalleryItem
-          key={id}
-          id={id}
-          tags={tags}
-          webImage={webformatURL}
-          largeImage={largeImageURL}
-        />
-      ))}
-    </Gallery>
-  );
+export const ImageGallery = ({ images, onClick }) => (
+  <Gallery>
+    {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+      <ImageGalleryItem
+        key={id}
+        url={webformatURL}
+        fullImage={largeImageURL}
+        tag={tags}
+        openModal={onClick}
+      />
+    ))}
+  </Gallery>
+);
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.objectOf).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
