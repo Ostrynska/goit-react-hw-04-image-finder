e063ad5b0170a5330fee218bcb975d9251ca2563
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-14 22:05:35 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,17 @@
+import PropTypes from 'prop-types';
+
 import { LoadMore } from './Button.styled';
 
-export const Button = ({ loadMore }) => {
+export const Button = ({ onLoadMore }) => {
   return (
-    <LoadMore type="button" onClick={loadMore}>
-      Load more
-    </LoadMore>
+    <>
+      <LoadMore type="button" onClick={onLoadMore}>
+        Load more
+      </LoadMore>
+    </>
   );
+};
+
+Button.propTypes = {
+  onLoadMore: PropTypes.func.isRequired,
 };
